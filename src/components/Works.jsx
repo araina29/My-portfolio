d3b0e2f149a1db97815651daa10946cd59d30c88
Aryaman Raina <105023486+araina25@users.noreply.github.com>
2024-01-24 21:31:56 -0700

    writing all my projects in

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import code from '../assets/code2.png';
+import code1 from '../assets/code3.png';
+import code2 from '../assets/Spotify.png';
+import code3 from '../assets/QR.png';
+import code4 from '../assets/P.png'
 
 const Works = () => {
   return (
@@ -21,16 +25,52 @@ const Works = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                Social Distribution
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'> Web application built with Vue and SQL</p>
               <div className='pt-8 text-center'>
-                <a href='/'>
+              
+                <a href='https://youtu.be/p-l-jmiMfUM'
+                target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Prevents security vulnerabilities
+              >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/araina29/404f23project-21-average.git'
+                target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Prevents security vulnerabilities
+              >
+                  
+                  
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                  
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+
+
+
+            style={{ backgroundImage: `url(${code1})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Pygame project
+              </span>
+              <p className='text-center'>A simple alien shooting game built on python</p>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/araina29/Pygame---Alien-Game.git'
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Prevents security vulnerabilities
+                >
+                  
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -39,22 +79,23 @@ const Works = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${code})` }}
+
+
+            style={{ backgroundImage: `url(${code2})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                MusicWiz
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>A Music recommendation system for spotify using sql and python</p>
               <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
+                <a href='https://github.com/BRBhacked/MUSICwiz.git'
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Prevents security vulnerabilities
+                >
+                  
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -63,22 +104,31 @@ const Works = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${code})` }}
+
+
+            style={{ backgroundImage: `url(${code3})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                Binary Bandits 
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>A mobile application developed on Android studio </p>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://github.com/CMPUT301W23T06/BinaryBandits/blob/main/README.md'
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Prevents security vulnerabilities
+                >
+
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/CMPUT301W23T06/BinaryBandits.git'
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Prevents security vulnerabilities
+                >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -87,46 +137,26 @@ const Works = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(${code4})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                My portfolio
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>Implemented on react/java</p>
               <div className='pt-8 text-center'>
                 <a href='/'>
+
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${code})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
-              <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
-              </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
+                <a href='https://github.com/araina29/My-portfolio.git'
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer" // Prevents security vulnerabilities
+                >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
