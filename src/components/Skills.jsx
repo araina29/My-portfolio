bd128ce79e19bdc64582e1e49315e61e675a61ec
Aryaman Raina <105023486+araina25@users.noreply.github.com>
2024-01-24 21:50:59 -0700

    correcting errors

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div className=' w-full flex justify-center items-center flex-col mb-7'>
               <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
-              <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
+              <p className='py-4 text-2xl'>I thrive on exploring and mastering new technologies and gaining new skills , below are some of my technical skills :</p>
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
