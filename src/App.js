61f1172382ae55979533d3e29317537d48a37f1e
Aryaman Raina <105023486+araina25@users.noreply.github.com>
2024-02-07 15:48:41 -0700

    updating work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from './components/About';
+import React from 'react';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
